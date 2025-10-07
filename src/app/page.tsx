0285c27a264334a09380120d3a0f09f33f74a4d6
Agustin Acosta <gonzaloagustin010u@gmail.com>
2025-10-07 21:07:18 +0000

    que la imagen tenga que ver con contabilidad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -76,12 +76,12 @@ export default function LoginPage() {
       </div>
       <div className="hidden bg-muted lg:block">
         <Image
-          src="https://picsum.photos/seed/taxwise/1920/1080"
+          src="https://picsum.photos/seed/accounting/1920/1080"
           alt="Image"
           width="1920"
           height="1080"
           className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-          data-ai-hint="accounting illustration"
+          data-ai-hint="accounting"
         />
       </div>
     </div>
